60985e744068c6fb6430bf326cd339c155e38d59
Aodhan Hamilton <hamilton.aodhan@gmail.com>
2024-11-28 18:31:03 +1300

    add type

--- src/inngest/sync-user.ts
+++ src/inngest/sync-user.ts
@@ -2,6 +2,12 @@ import { db } from '@/app/db'
 import { inngest } from './client'
 import { users } from '@/app/db/schema'
 
+interface EmailAddress {
+  id: string
+  email_address: string
+  // Add other properties as needed
+}
+
 export const syncUser = inngest.createFunction(
   { id: 'sync-user-from-clerk' }, // ←The 'id' is an arbitrary string used to identify the function in the dashboard
   { event: 'clerk/user.created' }, // ← This is the function's triggering event
@@ -9,7 +15,7 @@ export const syncUser = inngest.createFunction(
     const user = event.data // The event payload's data will be the Clerk User json object
     const { id, first_name } = user
     const email = user.email_addresses.find(
-      (e) => e.id === user.primary_email_address_id,
+      (e: EmailAddress) => e.id === user.primary_email_address_id,
     ).email_address
     await db.insert(users).values({ clerkId: id, name: first_name, email })
   },
